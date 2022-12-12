d516d1ecb7a70a0a2c4c1c77772b001daceb65be
ENevelskaia <116084075+ENevelskaia@users.noreply.github.com>
2022-12-13 00:30:40 +0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -4,43 +4,43 @@ CREATE TABLE IF NOT EXISTS Music_genre_list (
 	);
 
 CREATE TABLE IF NOT EXISTS Musician_list (
-	Musician_id SERIAL PRIMARY KEY,
-	Musician_name VARCHAR(30) NOT NULL
+	musician_id SERIAL PRIMARY KEY,
+	musician_name VARCHAR(30) NOT NULL
 	);
 
 CREATE TABLE IF NOT EXISTS Genre_musician (
 	genre_id INTEGER REFERENCES Music_genre_list(genre_id),
-	musician_id INTEGER REFERENCES Musician_list(Musician_id),
+	musician_id INTEGER REFERENCES Musician_list(musician_id),
 	CONSTRAINT Gen_mus_ID PRIMARY KEY (genre_id, musician_id)
     );
 
 CREATE TABLE IF NOT EXISTS Album_list (
-	Album_id SERIAL PRIMARY KEY,
-	Album_name VARCHAR(50) NOT null,
+	album_id SERIAL PRIMARY KEY,
+	album_name VARCHAR(50) NOT null,
 	release_year INTEGER NOT null
 	);
 
 CREATE TABLE IF NOT EXISTS Musician_album (
-	album_id INTEGER REFERENCES Album_list(Album_id),
-	musician_id INTEGER REFERENCES Musician_list(Musician_id),
+	album_id INTEGER REFERENCES Album_list(album_id),
+	musician_id INTEGER REFERENCES Musician_list(musician_id),
 	CONSTRAINT Mus_Al_ID PRIMARY KEY (album_id, musician_id)
 	);
 
 CREATE TABLE IF NOT EXISTS Trek_list (
-	Trek_id SERIAL PRIMARY KEY,
-	Album_id INTEGER NOT NULL REFERENCES Album_list (Album_id),
-	Trek_name VARCHAR(50) NOT null,
+	trek_id SERIAL PRIMARY KEY,
+	album_id INTEGER NOT NULL REFERENCES Album_list (album_id),
+	trek_name VARCHAR(50) NOT null,
 	trek_length TIME NOT null
 	);
 
 CREATE TABLE IF NOT EXISTS Collection (
-	Collection_id SERIAL PRIMARY KEY,
-	Collection_name VARCHAR(50) NOT null,
+	collection_id SERIAL PRIMARY KEY,
+	collection_name VARCHAR(50) NOT null,
 	release_year INTEGER NOT null
 	);
 
 CREATE TABLE IF NOT EXISTS Collection_trek (
-	collection_id INTEGER REFERENCES Collection (Collection_id),
-	trek_id INTEGER REFERENCES Trek_list(Trek_id),
+	collection_id INTEGER REFERENCES Collection (collection_id),
+	trek_id INTEGER REFERENCES Trek_list(trek_id),
 	CONSTRAINT Coll_tr_ID PRIMARY KEY (collection_id, trek_id)
 	);
