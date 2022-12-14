479613e3e46f35bf758881f35ca0b484d380d994
ENevelskaia <116084075+ENevelskaia@users.noreply.github.com>
2022-12-14 09:16:40 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS Employee_list (
 	emp_id SERIAL PRIMARY KEY,
 	name VARCHAR(80) NOT null,
 	department VARCHAR(80) NOT null, 
-	boss_id INTEGER FOREIGN KEY REFERENCES Employee_list (emp_id)
+	boss_id INTEGER REFERENCES Employee_list (emp_id)
 	);
