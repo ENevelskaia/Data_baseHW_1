e3270e29f8e56f304e9ce13619e4797e28a9d66c
ENevelskaia <116084075+ENevelskaia@users.noreply.github.com>
2022-12-15 21:41:33 +0300

    Add files via upload

---
+++ Script_HW_3_INSERT.sql
@@ -0,0 +1,118 @@
+INSERT INTO Music_genre_list (genre_name) VALUES
+    ('Jazz'),
+    ('Rock'),
+    ('Pop'),
+    ('Folk'),
+    ('Classic'),
+    ('R&B');
+
+INSERT INTO Musician_list (Musician_name) VALUES
+    ('Ella F'),
+    ('Michael J'),
+    ('Jen L'),
+    ('Lily R'),
+    ('Summer T'),
+    ('Govard M'),
+    ('Jimmy H'),
+    ('Mad');
+
+INSERT INTO Genre_musician VALUES
+    ('1', '1'),
+    ('2', '3'),
+    ('2', '4'),
+    ('3', '2'),
+    ('3', '5'),
+    ('4', '5'),
+    ('5', '6'),
+    ('6', '3'),
+    ('6', '8');
+
+INSERT INTO Album_list (Album_name, release_year) VALUES
+    ('First Album', '1986'),
+    ('Great Album', '2005'),
+    ('My Album', '2020'),
+    ('Strange Album', '2015'),
+    ('Yay Album', '2019'),
+    ('Just Album', '2018'),
+    ('Merry Album', '2013'),
+    ('Crazy Album', '2021'),
+    ('Last Album', '2022');
+   
+   
+INSERT INTO Musician_album VALUES
+    ('1', '1'),
+    ('2', '1'),
+    ('3', '2'),
+    ('4', '3'),
+    ('4', '4'),
+    ('5', '5'),
+    ('6', '6'),
+    ('7', '7'),
+    ('8', '8'),
+    ('9', '8');
+   
+INSERT INTO Trek_list (Album_id, Trek_name, trek_length) VALUES
+    ('1', 'My trek_1', '2:55'),
+    ('1', '1trek_2', '3:01'),
+    ('1', '1trek_3', '1:56'),
+    ('2', '2trek_1', '2:55'),
+    ('3', '3trek_1', '3:01'),
+    ('3', '3trek_2', '1:56'),
+    ('4', '4trek_1', '2:50'),
+    ('4', '4trek_2', '2:01'),
+    ('4', '4trek_3', '1:46'),
+    ('5', '5trek_1', '2:35'),
+    ('5', '5trek_2', '3:41'),
+    ('5', '5trek_3', '1:56'),
+    ('5', '5trek_4', '2:05'),
+    ('6', '6trek_1', '4:01'),
+    ('6', '6trek_2', '2:56'),
+    ('7', '7trek_1', '2:11'),
+    ('7', '7trek_2', '1:50'),
+    ('7', '7trek_3', '2:55'),
+    ('8', '8trek_1', '3:02'),
+    ('8', '8trek_2', '1:56'),
+    ('8', '8trek_3', '2:58'),
+    ('9', '9trek_1', '3:15');
+   
+INSERT INTO Collection (Collection_name, release_year) VALUES
+    ('Collection_1', '2014'),
+    ('Collection_2', '2016'),
+    ('Collection_3', '2015'),
+    ('Collection_4', '2018'),
+    ('Collection_5', '2019'),
+    ('Collection_6', '2017'),
+    ('Collection_7', '2011'),
+    ('Collection_8', '2022');
+   
+   
+INSERT INTO Collection_trek VALUES
+    ('1', '1'),
+    ('1', '6'),
+    ('1', '7'),
+    ('2', '3'),
+    ('2', '4'),
+    ('2', '15'),
+    ('3', '2'),
+    ('3', '5'),
+    ('3', '8'),
+    ('3', '14'),
+    ('4', '6'),
+    ('4', '10'),
+    ('4', '21'),
+    ('5', '3'),
+    ('5', '22'),
+    ('5', '17'),
+    ('6', '19'),
+    ('6', '21'),
+    ('6', '12'),
+    ('6', '16'),
+    ('7', '11'),
+    ('7', '18'),
+    ('7', '22'),
+    ('8', '4'),
+    ('8', '9'),
+    ('8', '1'),
+    ('8', '12'),
+    ('8', '20');
+   
---
+++ Script_HW_3_SELECT.sql
@@ -0,0 +1,18 @@
+SELECT Album_name, release_year  FROM Album_list
+WHERE release_year = 2018;
+
+select Trek_name, trek_length FROM Trek_list
+ORDER by trek_length DESC
+limit 1;
+
+SELECT Trek_name FROM Trek_list
+WHERE trek_length >= '3:30:00';
+
+SELECT Collection_name FROM Collection
+WHERE release_year BETWEEN 2018 AND 2020;
+
+SELECT Musician_name FROM Musician_list
+WHERE Musician_name NOT LIKE '% %';
+
+SELECT Trek_name FROM Trek_list
+WHERE Trek_name LIKE 'My %';
